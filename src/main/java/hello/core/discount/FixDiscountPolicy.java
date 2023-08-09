f576003d1d0a7f7e82811ddffaf233d2d784e045
SHEOMM <esh0821@snu.ac.kr>
2023-08-09 21:55:19 +0900

    feat : lombok을 적용한 OrderServiceImpl

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,6 +2,7 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
 public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount = 1000;
@@ -9,7 +10,9 @@ public class FixDiscountPolicy implements DiscountPolicy{
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP){
             return discountFixAmount;
+        }else {
+            return 0;
         }
-        return 0;
+
     }
 }

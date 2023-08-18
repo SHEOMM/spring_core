4ac3cf1deecdbd2473b66e2ce75de0bec8efb20a
SHEOMM <esh0821@snu.ac.kr>
2023-08-18 14:10:43 +0900

    feat : beanScope 생명주기 - 어노테이션 방식(권장)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,7 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -24,13 +26,13 @@ public class NetworkClient{
     public void disconnect() {
         System.out.println("close: " + url);
     }
-
+    @PostConstruct
     public void init() throws Exception {
         connect();
         call("초기화 연결 메시지");
     }
 
-
+    @PreDestroy
     public void close() throws Exception {
         disconnect();
     }
